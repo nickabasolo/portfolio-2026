01d77535e5da4bf6d00d80b5eb133135b7d2b1ac
nickabasolo <64856592+nickabasolo@users.noreply.github.com>
2026-03-20 02:59:03 -0400

    posthog tracking

--- index.tsx
+++ index.tsx
@@ -2,7 +2,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { PostHogProvider } from '@posthog/react';
 import App from './App';
+
+const posthogOptions = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  loaded: (ph: { opt_out_capturing: () => void }) => {
+    if (import.meta.env.DEV) ph.opt_out_capturing();
+  },
+};
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -12,8 +20,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={posthogOptions}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
